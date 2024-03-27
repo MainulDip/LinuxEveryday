6626ecefa41fbe939e6cb6398655238ca386db78
mainuldip <mainuldip@gmail.com>
2024-03-27 11:14:30 -0400

    linux bash user inputs

---
+++ BashScripting/bash-scripts/userinput.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo What is your first name?
+read FIRST_NAME # will wait for user input and will assign to FIRST_NAME variable
+
+echo Last Name?
+read LAST_NAME
+
+echo Your Full Name is $FIRST_NAME $LAST_NAME 
